33e8b5bc890a0558dccdbe43cf364f7b14f390f7
Amol Ghare <amolcghare309@gmail.com>
2021-10-17 11:04:18 +0530

    Test 2

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,10 +2,17 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-    	if(input.equals("")){
-    	    return 0;
-    	  }
-    	return 1;
-    }
+	public int add(String input) {
+		if (input.equals("")) {
+			return 0;
+		} else if (input.contains(",")) {
+			String[] numbers = input.split(",");
+			return toInt(numbers[0]) + toInt(numbers[1]);
+		} else
+			return 1;
+	}
+
+	private static int toInt(String number) {
+		return Integer.parseInt(number);
+	}
 }
